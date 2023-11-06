9e1ea05b71f877aabd5c12c9562ba6c562babf3f
Abhinav <abhinavajay20@gmail.com>
2023-11-06 19:31:52 +0530

    task4

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About</h2>
+      <p>Greetings, this is the portfolio of I, Abhinav Tomar.</p>
+      <p>This is also task 6 of react under Ethnus MERN Internship</p>
+    </section>
+  );
+}
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>abhinav.tomar2021a@vitstudent.ac.in</p>
+    </section>
+  );
+}
+
+export default Contact;
